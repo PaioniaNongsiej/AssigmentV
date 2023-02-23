5e5ed42d7e8ed2adab0da166fd0c6781bfa237bf
PaioniaNongsiej <126144173+PaioniaNongsiej@users.noreply.github.com>
2023-02-23 23:30:18 +0530

    Add files via upload

---
+++ Assignment2/Assignment2/Assigment2Part2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Assigment2Part2
+    {
+        public int AccNo;
+        public string CName;
+        public string AccType;
+        public string TransactionType;
+        public double amount;
+        public double balance = 5000;
+
+        public void Show()
+        {
+            Console.Write("\n__________________________________________________\n");
+            Console.Write("assignment2, part 2\nprogram to show account details\n");
+            Console.Write("__________________________________________________\n");
+
+            Console.WriteLine("Enter Account Number:");
+            this.AccNo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Customer's Name:");
+            this.CName = Console.ReadLine();
+            Console.WriteLine("Enter Account Type:");
+            this.AccType = Console.ReadLine();
+            Console.WriteLine("Enter Transaction Type (d->deposit / w->withdrawal):");
+            this.TransactionType = Console.ReadLine();
+            Console.WriteLine("Enter Amount:");
+            this.amount = Convert.ToDouble(Console.ReadLine());
+            if (TransactionType == "d")
+            {
+                TransactionType = "Deposit";
+                balance = balance + amount;
+            }
+            else if (TransactionType == "w" && amount < balance)
+            {
+                TransactionType = "Withdrawal";
+                balance = balance - amount;
+            }
+            else if (TransactionType == "w" && amount > balance)
+            {
+                TransactionType = "Transaction is not possible.";
+            }
+            Console.WriteLine($"\nAccount Number: { AccNo} \nCustomer's Name: {CName} \nAccount Type: {AccType} \nTansaction Type: { TransactionType} \nAmount: { amount} \nRemaining Balance: { balance}");
+        }
+    }
+}
---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.strlen();
+            p.strrev();
+            p.strcompare();
+            Assigment2Part2 acc = new Assigment2Part2();
+            acc.Show();
+            Console.ReadLine();
+        }
+
+        //finds length of the sring
+        public void strlen()
+        {
+            string str;
+            int l = 0;
+
+            Console.Write("Q1. length of a strings \n");
+            Console.Write("__________________________________________________\n");
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+
+            foreach (char ch in str)
+            {
+                l += 1;
+
+            }
+            Console.Write("\n\n");
+            Console.Write("Length of the string is : {0}\n", l);
+        }
+
+        //reverse of a string
+        public void strrev()
+        {
+            string Str, rev = "";
+            int Length;
+            Console.Write("__________________________________________________\n");
+            Console.Write("Q2. reverse of a string \n");
+            Console.Write("__________________________________________________\n");
+            Console.Write("Enter A String: ");
+            Str = Console.ReadLine();
+            Length = Str.Length - 1;
+            while (Length >= 0)
+            {
+                rev = rev + Str[Length];
+                Length--;
+            }
+            Console.WriteLine("Reverse  String::  {0} \t", rev);
+            Console.ReadLine();
+        }
+
+        //compare a string
+        public void strcompare()
+        {
+            string str1, str2;
+            Console.Write("__________________________________________________\n");
+            Console.Write("Q3. comparison of two strings \n");
+            Console.Write("__________________________________________________\n");
+            Console.WriteLine("Enter the First word");
+            str1 = Console.ReadLine();
+            Console.WriteLine("Enter the Another word");
+            str2 = Console.ReadLine();
+            if (str1 == str2)
+            {
+                Console.WriteLine("Same word");
+            }
+            else
+                Console.WriteLine("Not Same");
+        }
+    }
+}
