9c3aaa5cc69c6fdeb53ce45fec51cf96be15cdfa
PaioniaNongsiej <126144173+PaioniaNongsiej@users.noreply.github.com>
2023-02-27 23:47:55 +0530

    Add files via upload

---
+++ Assignment03/Assignment03/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                int count = 0;
+
+                Console.Write("program to display first name and last name to uppercase\n");
+                Console.Write("___________________________________________________________\n");
+                Console.Write("Enter your first name: ");
+                string firstName = Console.ReadLine();
+
+                Console.Write("Enter your last name: ");
+                string lastName = Console.ReadLine();
+
+                Display(firstName, lastName);
+
+                Console.Write("___________________________________________________________\n");
+                Console.Write("program to display number of occurance of a letter\n");
+                Console.Write("___________________________________________________________\n");
+
+                Console.Write("Enter a string: \n");
+                string str = Console.ReadLine();
+                Console.Write("Enter the letter to count: ");
+                char letter = char.Parse(Console.ReadLine());
+
+                count = CountLetterOccurrences(str, letter);
+                Console.WriteLine("The letter '{0}' appears {1} times in the string '{2}'.", letter, count, str);
+
+                Console.Read();
+            }
+            static void Display(string firstName, string lastName)
+            {
+                Console.WriteLine(firstName.ToUpper());
+                Console.WriteLine(lastName.ToUpper());
+                Console.Read();
+            }
+
+            static int CountLetterOccurrences(string str, char letter)
+            {
+                int count = 0;
+                foreach (char c in str)
+                {
+                    if (c == letter)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+    }
---
+++ Assignment03/Question3/Question3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student(1, "Paionia Nongsiej", "MCA", 4, "computer science");
+            s.DisplayData();
+            s.GetMarks();
+            s.DisplayResult();
+        }
+
+        public class Student
+        {
+            private int rollno;
+            private string name;
+            private string Sclass;
+            private int semester;
+            private string branch;
+            private int[] marks = new int[5];
+
+            public Student(int rollno, string name, string Sclass, int semester, string branch)
+            {
+                this.rollno = rollno;
+                this.name = name;
+                this.Sclass = Sclass;
+                this.semester = semester;
+                this.branch = branch;
+            }
+
+            public void GetMarks()
+            {
+                Console.WriteLine("Enter the marks for {0}:", name);
+                for (int i = 0; i < marks.Length; i++)
+                {
+                    Console.Write("Subject {0}: ", i+1);
+                    marks[i] = int.Parse(Console.ReadLine());
+                }
+                Console.ReadLine();
+            }
+
+            public void DisplayResult()
+            {
+                int sum = 0;
+                foreach (int mark in marks)
+                {
+                    sum += mark;
+                    if (mark < 35)
+                    {
+                        Console.WriteLine("Result: Failed");
+                        return;
+                    }
+                }
+
+                double average = sum / (double)marks.Length;
+
+                if (average < 50)
+                {
+                    Console.WriteLine("Result: Failed");
+                }
+                else
+                {
+                    Console.WriteLine("Result: Passed");
+                }
+            }
+
+            public void DisplayData()
+            {
+                Console.WriteLine("Roll No: {0}", rollno);
+                Console.WriteLine("Name: {0}", name);
+                Console.WriteLine("Class: {0}", Sclass);
+                Console.WriteLine("Semester: {0}", semester);
+                Console.WriteLine("Branch: {0}", branch);
+                Console.WriteLine("Marks:");
+            }
+        }
+    }
+}
