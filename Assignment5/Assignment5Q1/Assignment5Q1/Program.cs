b8c0385f1e3785dbf04ff6f9951594ac0001003d
PaioniaNongsiej <126144173+PaioniaNongsiej@users.noreply.github.com>
2023-03-02 10:45:34 +0530

    Add files via upload

---
+++ Assignment5/Assignment5Q1/Assignment5Q1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Undergraduate undergraduate = new Undergraduate();
+            undergraduate.Name = "Paionia Nongsiej";
+            undergraduate.StudentId = 92345;
+            undergraduate.Grade = 85.0;
+            Console.WriteLine("{0} Undergraduate is Passed: {1}", undergraduate.Name ,undergraduate.IsPassed(undergraduate.Grade));
+
+            Graduate graduate = new Graduate();
+            graduate.Name = "Elizabeth L. Mawphlang";
+            graduate.StudentId = 98900;
+            graduate.Grade = 78.0;
+            Console.WriteLine("{0} Graduate is Passed: {1}",graduate.Name, graduate.IsPassed(graduate.Grade));
+            Console.Read();
+        }
+        public abstract class Student
+        {
+            public string Name { get; set; }
+            public int StudentId { get; set; }
+            public double Grade { get; set; }
+
+            public abstract bool IsPassed(double grade);
+        }
+
+        public class Undergraduate : Student
+        {
+            public override bool IsPassed(double grade)
+            {
+                if (grade > 70.0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public class Graduate : Student
+        {
+            public override bool IsPassed(double grade)
+            {
+                if (grade > 80.0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Assignment5/Assignment5Q2/Assignment5Q2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dayscholar ds = new Dayscholar();
+            ds.ShowDetails();
+            Console.WriteLine("\n________________\n");
+            Resident rd = new Resident();
+            rd.ShowDetails();
+            Console.Read();
+        }
+        public interface IStudent
+        {
+            int StudentId { get; set; }
+            string Name { get; set; }
+            void ShowDetails();
+        }
+
+        public class Dayscholar : IStudent
+        {
+            public int StudentId { get; set; } = 1 ;
+            public string Name { get; set; } = "Paionia Nongsiej";
+            public float Fees { get; set; } = 50000;
+
+            public void ShowDetails()
+            {
+                Console.WriteLine($"Student ID: {StudentId}\nName: {Name}\nFees: {Fees}");
+            }
+        }
+
+        public class Resident : IStudent
+        {
+            public int StudentId { get; set; } = 2;
+            public string Name { get; set; } = "Elizabeth L. Mawphlang";
+            public float Fees { get; set; } = 100000;
+
+            public void ShowDetails()
+            {
+                Console.WriteLine($"Student ID: {StudentId}\nName: {Name}\nFees: {Fees}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
