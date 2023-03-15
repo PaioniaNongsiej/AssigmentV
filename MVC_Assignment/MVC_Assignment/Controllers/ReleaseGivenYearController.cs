5262bdb755e269cd0b8edc5b5dfa69ad50d22b31
Paionia Nongsiej <paionian@medusind.com>
2023-03-15 09:38:05 +0530

    ok

---
+++ MVC_Assignment/MVC_Assignment/Controllers/ReleaseGivenYearController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Assignment.Models;
+
+namespace MVC_Assignment.Controllers
+{
+    
+    public class ReleaseGivenYearController : Controller
+    {
+        // GET: ReleaseGivenYear
+        Movie db = new Movie();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getReleaseByYear()
+        {
+            List<Movie> movlist = (from m in db.Movies
+                                   where m.DateofRelease = "09/03/2023"
+                                       select m).ToList();
+            return View(movlist);
+
+        }
+    }
+}
---
+++ MVC_Assignment/MVC_Assignment/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC_Assignment.Models
+{
+   
+
+
+    public class Movie
+    {
+        [Key]
+        public int mid { get; set; }
+        public string MovieName { get; set; }
+        public DateTime DateOfRelease { get; set; }
+        public object Movie { get; internal set; }
+        public object Movie { get; internal set; }
+        public object Movies { get; internal set; }
+    }
+}
